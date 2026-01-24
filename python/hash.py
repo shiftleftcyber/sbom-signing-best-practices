936e0ed89184e15d2f89d6a09700f21d8728b4e1
Jason Smith <jsmith@shiftleftcyber.io>
2026-01-23 22:09:09 -0500

    Add ruff as a pre-commit check
    Use ruff linting, sorting and formatting
    
    Update hash.py and test_hash.py with linter fixes

--- python/hash.py
+++ python/hash.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 import jcs
 
+
 def compute_json_hash(input_bytes: bytes) -> str:
     # 1. Parse JSON
     data = json.loads(input_bytes)
@@ -15,13 +16,13 @@ def compute_json_hash(input_bytes: bytes) -> str:
             # Always remove the signature itself
             if "signature" not in exclusions:
                 exclusions.append("signature")
-            
+
             # Apply all exclusions
             for property_name in exclusions:
                 data.pop(property_name, None)
 
-    # 3. Apply RFC 8785 (JCS)
+    # 3. Apply RFC 8785 (JCS).
     canonical_bytes = jcs.canonicalize(data)
-    
+
     # 4. Hash the canonical bytes
     return hashlib.sha256(canonical_bytes).hexdigest()
